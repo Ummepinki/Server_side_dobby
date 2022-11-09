963514fa9217aa6325a7842182c5ab90ae48d48f
Ummepinki <salmaumme888@gmail.com>
2022-11-09 12:45:03 +0600

    create server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfgdtet.mongodb.net/?retryWrites=true&w=majority`;
+console.log("db connected");
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client.db("Image").collection("items");
+
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+
+    app.post("/items", async (req, res) => {
+      const newItem = req.body;
+      const result = await itemsCollection.insertOne(newItem);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running server");
+});
+
+app.listen(port, () => {
+  console.log("listen to port ", port);
+});
